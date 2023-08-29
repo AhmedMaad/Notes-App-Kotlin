d547ad41504465998caff087473a7ae52839a1f5
AhmedMaad <atef.ahmed1341@gmail.com>
2023-08-30 01:57:02 +0200

    Adding view binding to recycler view adapter

--- app/src/main/java/com/maad/notesappkotlin/home/NoteAdapter.kt
+++ app/src/main/java/com/maad/notesappkotlin/home/NoteAdapter.kt
@@ -10,28 +10,26 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.maad.notesappkotlin.R
 import com.maad.notesappkotlin.database.Note
+import com.maad.notesappkotlin.databinding.NoteListItemBinding
 import com.maad.notesappkotlin.edit.EditNoteActivity
 
 class NoteAdapter(private val activity: Activity, private val notes: ArrayList<Note>) :
     Adapter<NoteAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(view: View): ViewHolder(view) {
-        val noteContainer: CardView = view.findViewById(R.id.note_container_cv)
-        val noteDetails: TextView = view.findViewById(R.id.note_details_tv)
-    }
+    class NoteViewHolder(val binding: NoteListItemBinding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val view = activity.layoutInflater.inflate(R.layout.note_list_item, parent, false)
-        return NoteViewHolder(view)
+        //Reference: https://stackoverflow.com/questions/68693109/using-viewbinding-in-recyclerview-adapter-wierldly-displays-items
+        val binding = NoteListItemBinding.inflate(activity.layoutInflater, parent, false)
+        return NoteViewHolder(binding)
     }
 
     override fun getItemCount() = notes.size
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        holder.noteDetails.text = notes[position].noteDetails
-
-        holder.noteContainer.setOnClickListener {
-            val  i = Intent(activity, EditNoteActivity::class.java)
+        holder.binding.noteDetailsTv.text = notes[position].noteDetails
+        holder.binding.noteContainerCv.setOnClickListener {
+            val i = Intent(activity, EditNoteActivity::class.java)
             i.putExtra("note", notes[position])
             activity.startActivity(i)
         }
